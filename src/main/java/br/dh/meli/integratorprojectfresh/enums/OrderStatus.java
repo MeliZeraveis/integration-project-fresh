82f8eda16ed883e5f2f42651d29510092d731169
Marcello Pires Alves <marcello.alves@gmail.com>
2022-11-10 17:16:11 -0300

    feat: new messages in Msg feat: added validation dependencies refactor: corrected OrderStatus & order route

--- src/main/java/br/dh/meli/integratorprojectfresh/enums/OrderStatus.java
+++ src/main/java/br/dh/meli/integratorprojectfresh/enums/OrderStatus.java
@@ -3,8 +3,8 @@ package br.dh.meli.integratorprojectfresh.enums;
 import org.hibernate.criterion.Order;
 
 public enum OrderStatus {
-  PENDING("Pending"),
-  APPROVED("Approved");
+  PENDING("Aberto"),
+  APPROVED("Finalizado");
 
   OrderStatus(String status) {
   }
