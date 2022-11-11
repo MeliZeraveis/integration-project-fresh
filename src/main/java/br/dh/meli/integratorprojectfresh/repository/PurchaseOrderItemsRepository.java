a3b2ec1b4b31698b508714c2f7fe79a488884500
Marcello Pires Alves <marcello.alves@gmail.com>
2022-11-11 17:38:51 -0300

    feat: service layer

--- src/main/java/br/dh/meli/integratorprojectfresh/repository/PurchaseOrderItemsRepository.java
+++ src/main/java/br/dh/meli/integratorprojectfresh/repository/PurchaseOrderItemsRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface PurchaseOrderItemsRepository extends JpaRepository<PurchaseOrderItems, Long> {
   List<PurchaseOrderItems> findByPurchaseOrderId(Long id);
+  void deleteByPurchaseOrderId(Long id);
 }
