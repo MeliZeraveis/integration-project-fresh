5c71c7eff06c65d09c6d88485bb34178f424e563
Marcello Pires Alves <marcello.alves@gmail.com>
2022-11-11 14:19:51 -0300

    feat: added constructors to Purchase Order models

--- src/main/java/br/dh/meli/integratorprojectfresh/repository/PurchaseOrderItemsRepository.java
+++ src/main/java/br/dh/meli/integratorprojectfresh/repository/PurchaseOrderItemsRepository.java
@@ -4,6 +4,9 @@ import br.dh.meli.integratorprojectfresh.model.PurchaseOrderItems;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PurchaseOrderItemsRepository extends JpaRepository<PurchaseOrderItems, Long> {
+  List<PurchaseOrderItems> findByPurchaseOrderId(Long id);
 }
