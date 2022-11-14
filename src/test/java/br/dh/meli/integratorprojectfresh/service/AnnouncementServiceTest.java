a126fa02b6b560be81332a1218cbcb353dda8ae0
EstherPimentel <esther.epimentel@mercadolivre.com>
2022-11-14 15:25:56 -0300

    AnnoucementServiceTest in progress

--- src/test/java/br/dh/meli/integratorprojectfresh/service/AnnouncementServiceTest.java
+++ src/test/java/br/dh/meli/integratorprojectfresh/service/AnnouncementServiceTest.java
@@ -4,10 +4,7 @@ import br.dh.meli.integratorprojectfresh.dto.request.SectionDTO;
 import br.dh.meli.integratorprojectfresh.dto.response.AnnoucementGetResponseDTO;
 import br.dh.meli.integratorprojectfresh.enums.Msg;
 import br.dh.meli.integratorprojectfresh.exception.NotFoundException;
-import br.dh.meli.integratorprojectfresh.model.Announcement;
-import br.dh.meli.integratorprojectfresh.model.BatchStock;
-import br.dh.meli.integratorprojectfresh.model.Section;
-import br.dh.meli.integratorprojectfresh.model.User;
+import br.dh.meli.integratorprojectfresh.model.*;
 import br.dh.meli.integratorprojectfresh.repository.AnnouncementRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,6 +18,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -46,18 +45,27 @@ class AnnouncementServiceTest {
 
     Section section;
 
+    Warehouse warehouse;
+
     @BeforeEach
     void setUp() {
+        LocalDate manufacturingDate = LocalDate.parse("2021-12-12");
         LocalDate dueDate1 = LocalDate.parse("2021-10-10");
         LocalDate dueDate2 = LocalDate.parse("2021-11-11");
-
-        User user = new User(1L, "teste", "1234", "teste@email.com", "seller");
+        LocalDate dueDate3 = LocalDate.parse("2021-11-11");
+        LocalDateTime manufacturingTime = LocalDateTime.parse("2020-03-09 17:55:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        //User user1 = new User(1L, "teste", "1234", "teste@email.com", "seller");
+        //User user2 = new User(5L, "teste", "1234", "teste@email.com", "manager");
+        batchStock = new BatchStock(1L, "Fresh", 10, manufacturingDate, manufacturingTime, 10.0f, dueDate3, BigDecimal.valueOf(30.50));
+        List<BatchStock>batchStockList2 = List.of(batchStock);
+        warehouse = new Warehouse(1L, "teste", "1234", "12", new ArrayList<>(), null );
         section = new Section(1L, "Fresh", 50.0f, 20.0f, null, null);
-        announcement = new Announcement(1L, "Camisa", "Camisa branca", 4L, BigDecimal.valueOf(100.0), 1L, section, new ArrayList<>(), null, new ArrayList<>());
+        announcement = new Announcement(1L, "Camisa", "Camisa branca", 4L, BigDecimal.valueOf(100.0), 1L, section, batchStockList2, null, new ArrayList<>());
         sectionDTO = new SectionDTO(1L, 1L);
         BatchSotckAnnoucementDTO batchStockDTO1 = new BatchSotckAnnoucementDTO(1L, 10, dueDate1);
         BatchSotckAnnoucementDTO batchStockDTO2 = new BatchSotckAnnoucementDTO(2L, 12, dueDate2);
         batchStockList = List.of(batchStockDTO1, batchStockDTO2);
+
         responseDTO = new AnnoucementGetResponseDTO(sectionDTO, 1L, batchStockList);
 
     }
