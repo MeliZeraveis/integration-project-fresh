90281db95156e5242054dd1ac15659efba461911
Bruna Campos <bruna.camposf@mercadolivre.com>
2022-11-14 15:44:39 -0300

    TEST(Get) announcementService setup incomplet

--- src/test/java/br/dh/meli/integratorprojectfresh/service/AnnouncementServiceTest.java
+++ src/test/java/br/dh/meli/integratorprojectfresh/service/AnnouncementServiceTest.java
@@ -1,5 +1,7 @@
 package br.dh.meli.integratorprojectfresh.service;
 import br.dh.meli.integratorprojectfresh.dto.request.BatchSotckAnnoucementDTO;
+import br.dh.meli.integratorprojectfresh.dto.request.BatchStockDTO;
+import br.dh.meli.integratorprojectfresh.dto.request.InboundOrderDTO;
 import br.dh.meli.integratorprojectfresh.dto.request.SectionDTO;
 import br.dh.meli.integratorprojectfresh.dto.response.AnnoucementGetResponseDTO;
 import br.dh.meli.integratorprojectfresh.enums.Msg;
@@ -36,6 +38,10 @@ class AnnouncementServiceTest {
     AnnoucementGetResponseDTO responseDTO;
 
     List<BatchSotckAnnoucementDTO> batchStockList;
+    InboundOrderDTO inboundOrderDTO;
+    InboundOrder inboundOrder;
+    List<BatchStockDTO> batchStockListDTO;
+
 
     Announcement announcement;
 
@@ -53,11 +59,29 @@ class AnnouncementServiceTest {
         LocalDate dueDate1 = LocalDate.parse("2021-10-10");
         LocalDate dueDate2 = LocalDate.parse("2021-11-11");
         LocalDate dueDate3 = LocalDate.parse("2021-11-11");
+        LocalDate orderDate = LocalDate.parse("2022-03-03");
         LocalDateTime manufacturingTime = LocalDateTime.parse("2020-03-09 17:55:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
         //User user1 = new User(1L, "teste", "1234", "teste@email.com", "seller");
         //User user2 = new User(5L, "teste", "1234", "teste@email.com", "manager");
+
+        BatchStockDTO batchStock1 = new BatchStockDTO(1L, "Fresh", 10, manufacturingDate, manufacturingTime, (float) 1.5, dueDate1, BigDecimal.valueOf(30.5));
+        BatchStockDTO batchStock2 = new BatchStockDTO(1L, "Fresh", 11, manufacturingDate, manufacturingTime, (float) 1.3, dueDate2, BigDecimal.valueOf(20.5));
+
+        batchStockListDTO = new ArrayList<>();
+        batchStockListDTO.add(batchStock1);
+        batchStockListDTO.add(batchStock2);
+
+
+
+        inboundOrderDTO = new InboundOrderDTO(orderDate, 1L, 1L, batchStockListDTO);
+        inboundOrder = new InboundOrder(inboundOrderDTO, 1L);
+
+
+
+
         batchStock = new BatchStock(1L, "Fresh", 10, manufacturingDate, manufacturingTime, 10.0f, dueDate3, BigDecimal.valueOf(30.50));
-        List<BatchStock>batchStockList2 = List.of(batchStock);
+        List<BatchStock> batchStockList2 = new ArrayList<>();
+        batchStockList2.add(batchStock);
         warehouse = new Warehouse(1L, "teste", "1234", "12", new ArrayList<>(), null );
         section = new Section(1L, "Fresh", 50.0f, 20.0f, null, null);
         announcement = new Announcement(1L, "Camisa", "Camisa branca", 4L, BigDecimal.valueOf(100.0), 1L, section, batchStockList2, null, new ArrayList<>());
