8dd4eb8adf0023da233c183a4e4068b10c7f4ba1
Marcello Pires Alves <marcello.alves@gmail.com>
2022-11-17 09:37:40 -0300

    chore: add @Repository annotaton to repos

--- src/main/java/br/dh/meli/integratorprojectfresh/repository/BatchStockRepository.java
+++ src/main/java/br/dh/meli/integratorprojectfresh/repository/BatchStockRepository.java
@@ -2,6 +2,8 @@ package br.dh.meli.integratorprojectfresh.repository;
 
 import br.dh.meli.integratorprojectfresh.model.BatchStock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BatchStockRepository extends JpaRepository<BatchStock, Long> {
 }
--- src/main/java/br/dh/meli/integratorprojectfresh/repository/SectionRepository.java
+++ src/main/java/br/dh/meli/integratorprojectfresh/repository/SectionRepository.java
@@ -2,6 +2,8 @@ package br.dh.meli.integratorprojectfresh.repository;
 
 import br.dh.meli.integratorprojectfresh.model.Section;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SectionRepository extends JpaRepository<Section, Long> {
 }
