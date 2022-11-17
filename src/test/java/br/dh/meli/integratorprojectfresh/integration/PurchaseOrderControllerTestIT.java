9f83d1b7b056b3922439f593d661cd828fb0380d
Bruna Campos <bruna.camposf@mercadolivre.com>
2022-11-17 20:11:02 -0300

    TEST method save sucess is ok

--- src/test/java/br/dh/meli/integratorprojectfresh/integration/PurchaseOrderControllerTestIT.java
+++ src/test/java/br/dh/meli/integratorprojectfresh/integration/PurchaseOrderControllerTestIT.java
@@ -1,8 +1,12 @@
 package br.dh.meli.integratorprojectfresh.integration;
 
 
+import br.dh.meli.integratorprojectfresh.dto.request.PurchaseOrderRequestDTO;
 import br.dh.meli.integratorprojectfresh.dto.response.PurchaseOrderResponseDTO;
+import br.dh.meli.integratorprojectfresh.enums.OrderStatus;
 import br.dh.meli.integratorprojectfresh.model.PurchaseOrder;
+import br.dh.meli.integratorprojectfresh.model.PurchaseOrderItems;
+import br.dh.meli.integratorprojectfresh.repository.PurchaseOrderItemsRepository;
 import br.dh.meli.integratorprojectfresh.repository.PurchaseOrderRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.CoreMatchers;
@@ -13,10 +17,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -31,22 +43,40 @@ public class PurchaseOrderControllerTestIT {
     private MockMvc mockMvc;
 
     @Autowired
-    private PurchaseOrderRepository repo;
+    private PurchaseOrderRepository orderRepo;
+    @Autowired
+    private PurchaseOrderItemsRepository itemsRepo;
 
     @Autowired
     private ObjectMapper objectMapper;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     PurchaseOrder purchaseOrder;
 
     PurchaseOrderResponseDTO purchaseOrderResponseDTO;
+    PurchaseOrderRequestDTO purchaseOrderRequestDTO;
 
     @BeforeEach
     void setup() {
+        itemsRepo.deleteAll();
+        orderRepo.deleteAll();
 
+        jdbcTemplate.execute("ALTER TABLE purchase_order AUTO_INCREMENT = 1");
+        jdbcTemplate.execute("ALTER TABLE purchase_order_items AUTO_INCREMENT = 1");
+
+        LocalDateTime date = LocalDateTime.parse("2020-03-09 17:55:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        PurchaseOrder purchaseOrder = new PurchaseOrder(date, OrderStatus.PENDING, new BigDecimal("100.00"), 1L);
+        PurchaseOrderItems purchaseOrderItems = new PurchaseOrderItems(1L,1L, 10, new BigDecimal("10.00"));
+        List<PurchaseOrderItems> purchaseOrderItemsList = List.of(purchaseOrderItems);
+
+        purchaseOrderRequestDTO = new PurchaseOrderRequestDTO(date, 1L, OrderStatus.PENDING, purchaseOrderItemsList);
+        orderRepo.save(purchaseOrder);
+        itemsRepo.saveAll(purchaseOrderItemsList);
     }
 
     @Test
-    @DisplayName("Testa se o metodo retorna o anuncio correto em caso de sucesso")
+    @DisplayName("")
     void get_ReturnannoucementGetResponseDTO_Sucess() throws Exception {
 
         ResultActions response = mockMvc
@@ -59,4 +89,22 @@ public class PurchaseOrderControllerTestIT {
                 .andExpect(jsonPath("$.productId", CoreMatchers.is(1)));
 
     }
+
+    @Test
+    @DisplayName("depois vejo")
+    void SaveMethod_ReturnPurchaseOrderResponseDTO_WhenSucess() throws Exception {
+
+        ResultActions response = mockMvc.perform(post("/api/v1/fresh-product/orders")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(purchaseOrderRequestDTO)));
+
+        response.andExpect(status().isCreated());
+
+//        assertThat(orderRepo.findAll().size()).isEqualTo(2);
+//        assertThat(orderRepo.findAll().get(0)).isNotNull();
+//        assertThat(orderRepo.findAll().size()).isEqualTo(4);
+//        assertThat(orderRepo.findAll().get(0)).isNotNull();
+
+    }
+
 }
