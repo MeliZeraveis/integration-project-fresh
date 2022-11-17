72677722ed2a0d6046d1f2f1e9c887f73c91492c
Marcello Pires Alves <marcello.alves@gmail.com>
2022-11-17 16:16:01 -0300

    feat: business rule exception handler feat: internal server error exception handler

---
+++ src/main/java/br/dh/meli/integratorprojectfresh/exception/BusinessRuleException.java
@@ -0,0 +1,15 @@
+package br.dh.meli.integratorprojectfresh.exception;
+
+/**
+ * The type Business rule exception.
+ */
+public class BusinessRuleException extends RuntimeException {
+
+  /**
+   * Instantiates a new Business rule exception.
+   * @param message the message
+   */
+  public BusinessRuleException(String message) {
+    super(message);
+  }
+}
