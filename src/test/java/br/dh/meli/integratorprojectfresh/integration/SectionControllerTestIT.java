0332e8ea74d5a404173511f8a6eeb4278f2be760
jbenevides92 <jonatas.benevides@mercadolivre.com>
2022-11-22 16:19:06 -0300

    adding integration test

--- src/test/java/br/dh/meli/integratorprojectfresh/integration/SectionControllerTestIT.java
+++ src/test/java/br/dh/meli/integratorprojectfresh/integration/SectionControllerTestIT.java
@@ -1,6 +1,9 @@
 package br.dh.meli.integratorprojectfresh.integration;
 
 import br.dh.meli.integratorprojectfresh.dto.request.SectionRequestDTO;
+import br.dh.meli.integratorprojectfresh.dto.response.SectionDetailResponseDTO;
+import br.dh.meli.integratorprojectfresh.dto.response.SectionListFilterResponseDTO;
+import br.dh.meli.integratorprojectfresh.dto.response.SectionListWarehouseResponseDTO;
 import br.dh.meli.integratorprojectfresh.dto.response.SectionResponseDTO;
 import br.dh.meli.integratorprojectfresh.enums.ExceptionType;
 import br.dh.meli.integratorprojectfresh.enums.Msg;
@@ -18,6 +21,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
@@ -25,6 +29,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -51,9 +56,15 @@ public class SectionControllerTestIT {
     private JdbcTemplate jdbcTemplate;
 
     User userTest;
-    Warehouse warehouseTest;
-    Section sectionTest;
+    SectionResponseDTO sectionResponseTest;
     SectionRequestDTO sectionRequestTest;
+    SectionDetailResponseDTO sectionDetailResponseTest;
+    SectionListFilterResponseDTO sectionFilterResponseTest;
+    List<SectionListFilterResponseDTO> sectionListFilterResponseTest;
+    SectionListWarehouseResponseDTO sectionListWarehouseResponseTest;
+    Section sectionTest;
+    Warehouse warehouseTest;
+    String orderBy;
 
 
     @BeforeEach
@@ -62,9 +73,20 @@ public class SectionControllerTestIT {
         jdbcTemplate.execute("ALTER TABLE section AUTO_INCREMENT = 1");
 
         userTest = new User(1L,"test","123456","test@email.com", Roles.MANAGER);
-        warehouseTest = new Warehouse(1L,"Teste","AddressTest","T",new ArrayList<>(),new ArrayList<>(),userTest);
-        sectionTest = new Section(1L, "a", 50.0f, 20.0f, 1L, new ArrayList<>(), new ArrayList<>(), warehouseTest);
+        warehouseTest = new Warehouse(1L,"Teste","AddressTest","TR",new ArrayList<>(),new ArrayList<>(),userTest);
+        sectionTest = new Section(1L, "Fresh", 50.0f, 20.0f, 1L, new ArrayList<>(), new ArrayList<>(), warehouseTest);
+        Section sectionTest2 = new Section(2L, "Refrigerated", 50.0f, 20.0f, 1L, new ArrayList<>(), new ArrayList<>(), warehouseTest);
         sectionRequestTest = new SectionRequestDTO(sectionTest);
+        sectionResponseTest = new SectionResponseDTO(sectionTest);
+        sectionDetailResponseTest = new SectionDetailResponseDTO(sectionTest);
+        sectionFilterResponseTest = new SectionListFilterResponseDTO(sectionTest);
+        SectionListFilterResponseDTO sectionFilterResponseTest2 = new SectionListFilterResponseDTO(sectionTest2);
+
+        sectionListFilterResponseTest = new ArrayList<>();
+        sectionListFilterResponseTest.add(sectionFilterResponseTest);
+        sectionListFilterResponseTest.add(sectionFilterResponseTest2);
+        orderBy = "desc";
+        sectionListWarehouseResponseTest = new SectionListWarehouseResponseDTO(warehouseTest, orderBy);
 
         sectionRepo.save(sectionTest);
     }
@@ -73,7 +95,7 @@ public class SectionControllerTestIT {
     @DisplayName("Erro ao enviar SectionType invalido Save/Post")
     void SaveMethod_ReturnPurchaseOrderResponseDTO_WhenSectionTypeIsInvalid() throws Exception {
 
-        sectionTest.setType("a");
+        sectionRequestTest.setType("a");
 
         ResultActions response = mockMvc.perform(post("/api/v1/fresh-products/sections")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -85,5 +107,57 @@ public class SectionControllerTestIT {
                 .andExpect(jsonPath("$.fields", CoreMatchers.containsString("type")))
                 .andExpect(jsonPath("$.fieldsMessages", CoreMatchers.containsString(Msg.SECTION_NOT_VALID)));
     }
+
+    @Test
+    @DisplayName("Not Found Exception - GET")
+    void GetSectionByIdMethod_ReturnSectionDetailResponseDTO_WhenIdIsInvalid() throws Exception {
+
+        ResultActions response = mockMvc
+                .perform(get("/api/v1/fresh-products/sections/{id}",99L)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print());
+
+
+        response.andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.title", CoreMatchers.is(ExceptionType.OBJECT_NOT_FOUND.name())))
+                .andExpect(jsonPath("$.message", CoreMatchers.is(Msg.SECTION_NOT_FOUND)))
+                .andExpect(jsonPath("$.status", CoreMatchers.is(HttpStatus.NOT_FOUND.value())));
+    }
+
+    @Test
+    @DisplayName("Not Found Exception - GET")
+    void GetSectionListMethod_ReturnSectionDetailResponseDTO_WhenWarehouseIsInvalid() throws Exception {
+
+        ResultActions response = mockMvc
+                .perform(get("/api/v1/fresh-products/sections")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("warehouseName", "id")
+                        .param("orderBy","desc"))
+                .andDo(print());
+
+        response.andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.title", CoreMatchers.is(ExceptionType.OBJECT_NOT_FOUND.name())))
+                .andExpect(jsonPath("$.message", CoreMatchers.is(Msg.WAREHOUSE_NOT_FOUND)))
+                .andExpect(jsonPath("$.status", CoreMatchers.is(HttpStatus.NOT_FOUND.value())));
+    }
+
+    @Test
+    @DisplayName("Not Found Exception - GET")
+    void GetSectionListMethod_ReturnSectionDetailResponseDTO_WhenOrderByIsInvalid() throws Exception {
+
+        ResultActions response = mockMvc
+                .perform(get("/api/v1/fresh-products/sections")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("warehouseName", "MLBSP01")
+                        .param("orderBy","lll"))
+                .andDo(print());
+
+        response.andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.title", CoreMatchers.is(ExceptionType.PARAMETER_NOT_VALID.name())))
+                .andExpect(jsonPath("$.message", CoreMatchers.is(Msg.ORDER_TYPE_NOT_FOUND)))
+                .andExpect(jsonPath("$.status", CoreMatchers.is(HttpStatus.BAD_REQUEST.value())));
+    }
+
+
 
 }
