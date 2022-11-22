a2cc111d99e64e5dc8463b92d0eba57b8267b0c9
Marcello Pires Alves <marcello.alves@gmail.com>
2022-11-21 22:32:30 -0300

    refactor: outbound order repository

--- src/main/java/br/dh/meli/integratorprojectfresh/repository/OutboundOrderBatchesRepository.java
+++ src/main/java/br/dh/meli/integratorprojectfresh/repository/OutboundOrderBatchesRepository.java
@@ -4,9 +4,18 @@ import br.dh.meli.integratorprojectfresh.model.OutboundOrderBatches;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * The interface Outbound order batches repository.
  */
 @Repository
 public interface OutboundOrderBatchesRepository extends JpaRepository<OutboundOrderBatches, Long> {
+  /**
+   * Find by order number id list.
+   *
+   * @param id the id
+   * @return the list
+   */
+  List<OutboundOrderBatches> findAllByOrderNumberId(Long id);
 }
